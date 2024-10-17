8bb4ffd7b3319f72996d19961d444cf490cf0305
Võ Nguyên Gia Bảo <21127017@student.hcmus.edu.vn>
2024-10-17 09:12:17 +0700

    adjusted handler provider

--- internal/handler/rest/v1/handler.go
+++ internal/handler/rest/v1/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fintrack/internal/handler/rest/v1/currency"
 	"fintrack/internal/handler/rest/v1/transaction"
 	"fintrack/internal/handler/rest/v1/user"
 
@@ -11,4 +12,5 @@ import (
 var ProviderSetHandler = wire.NewSet(
 	user.NewUserHandler,
 	transaction.NewTransactionHandler,
+	currency.NewCurrencyHandler,
 )
